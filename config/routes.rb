b2ba3a5b69da904a65ce1d0c5ac3b601da5c4cd5
Reto Stamm <reto.stamm@originate.com>
2013-08-09 15:09:28 -0700

    Added default format option xml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resource :robocall, :only => [:connected_to_caller] do
-    post '/:id/:token' => 'robocall/robocalls#connected_to_caller'
+    post '/:id/:token' => 'robocall/robocalls#connected_to_caller', :defaults => { :format => 'xml' }
   end
 end
--- spec/routing/robocall/robocalls_routing_spec.rb
+++ spec/routing/robocall/robocalls_routing_spec.rb
@@ -5,7 +5,7 @@ module Robocall
     describe 'routing' do
   
       it 'routes to #post' do
-        post('/robocall/1/123a').should route_to('robocall/robocalls#connected_to_caller', :id => '1', :token => '123a')
+        post('/robocall/1/123a').should route_to('robocall/robocalls#connected_to_caller', :id => '1', :token => '123a', :format=> 'xml')
       end
     end
   end
