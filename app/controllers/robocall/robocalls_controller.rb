f36a82cd51701ad89c5668fbb55390fdcc53c6eb
Reto Stamm <reto.stamm@originate.com>
2013-08-12 14:25:25 -0700

    0.1.1 fixes spoken error messages

--- app/controllers/robocall/robocalls_controller.rb
+++ app/controllers/robocall/robocalls_controller.rb
@@ -20,14 +20,13 @@ module Robocall
       if error != ''
         @error = error
         template = <<'HAML'
-<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-
-%Say{:voice => 'alice'}
-  An error has occured retreieveing your automatic message.
-  Specifically,
-  = error
+<?xml version='1.0' encoding='UTF-8'?>
+%Response
+  %Say{:voice => 'alice'}
+    An error has occured retreieveing your automatic message.
+    Specifically,
+    = error
 HAML
-
         xml = Haml::Engine.new(template).to_html(Object.new, {:error => error} )
         render :xml => xml, :content_type => 'application/xml'
       else
